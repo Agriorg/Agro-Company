04e11f52abef60b598ef394b576cea4c4c0c2dc1
akshatj22 <akshat22j@gmail.com>
2024-02-10 18:36:43 +0530

    wewe

--- src/components/MissionVision.js
+++ src/components/MissionVision.js
@@ -2,15 +2,31 @@ import React from 'react'
 
 function MissionVision() {
   return (
-    <div className='container' style={{height:'20rem',marginTop:'7.3rem',paddingTop:'15px'}}>
+    <div className='container' style={{height:'40rem',marginTop:'7.3rem',paddingTop:'15px'}}>
         <div className='container'>
         <h2 className='main-title text-center'>Vision</h2>
-        <p>To emerge as the global leader in agricultural goods, establishing the benchmark for quality,
+        <p style={{paddingLeft:'39px'}}>To emerge as the global leader in agricultural goods, establishing the benchmark for quality,
              innovation, and sustainability, while fostering a positive and meaningful impact within every community 
              along the value chain worldwide.</p>
         <h2 className='text-center main-title'>Mission</h2>
-        <p></p>
-        <h2 className='text-center main-title'>Values</h2>
+        <p style={{paddingLeft:'39px'}}>To emerge as the global leader in agricultural goods, establishing the benchmark for quality,
+           innovation, and sustainability, while fostering a positive and meaningful impact within every community along
+            the value chain worldwide.</p>
+            <div>
+              <h2 className='text-center main-title'>Values</h2>
+              <ul>
+                <h3>Consumer Centric Approach</h3>
+                <li>Listening attentively to market needs, we foster a culture of continuous innovation, 
+                  ensuring our products consistently exceed consumer expectations.</li>
+                  <h3>Quality First</h3>
+                  <li>We uphold the highest standards in both product quality and agricultural processes, delivering nothing less than the best.</li>
+                  <h3>Collaberative Excellence</h3>
+                  <li>We thrive on collaboration, working hand-in-hand with farmers to cultivate 
+                    not just crops but sustainable partnerships. Our dedication to quality agricultural technology 
+                    ensures that what goes into our products is synonymous with what our customers receive – 
+                    excellence, through and through.</li>
+              </ul>
+            </div>
         <p></p>
         </div>
     </div>
