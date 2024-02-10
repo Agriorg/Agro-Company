5ad0ec10ecc930331cd726e8e7d651fdd6e7e591
akshatj22 <akshat22j@gmail.com>
2024-02-11 00:40:46 +0530

    wffs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-scroll';
 import image from "../img/Image.png";
 
 function Header() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleButtonClick = () => {
-    navigate('/about');
-  };
+  // const handleButtonClick = () => {
+  //   navigate('about');
+  // };
 
   return (
     <div className='header'>
       <div className='headertext'>
         <h1>Discover The Best Agricultural Products</h1>
         <p>Products for your daily Household needs</p>
-        {/* Use onClick to call handleButtonClick */}
-        <button className='headbtn' onClick={handleButtonClick}>Let's Explore</button>
+        <Link
+          to="about"
+          smooth={true}
+          duration={1000}
+          className="headbtn"
+        >
+          Let's Explore
+        </Link>
+        {/* <button className='headbtn' onClick={handleButtonClick}>Let's Explore</button> */}
       </div>
       <div className='headerimg'>
         <img src={image} alt="Farmer" className='farmerimg' />
