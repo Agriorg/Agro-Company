91842995b8240c2ca00c1b4b7a2887ca50371e82
akshatj22 <akshat22j@gmail.com>
2024-02-12 22:19:14 +0530

    fdsf

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ function Card(props) {
       <br />
       
       <div className="text-center">
-        <img alt="card-img" src={props.img} className="text-center img-fluid"   style={{ height: "150px", width:'150px', borderRadius: "20px" }} />
+        <img alt="card-img" src={props.img} className="text-center img-fluid"   style={{ height: "150px", width:'230px', borderRadius: "20px" }} />
       </div>
       <div className="text-center">
         <h3 className="card-title">{props.title}</h3>
--- src/components/OnionPowder.js
+++ src/components/OnionPowder.js
@@ -9,19 +9,19 @@ function OnionPowder() {
           {
         title: "Dehydrated Onion Powder",
         img: DeOnion,
-        text:'Introducing our premium Dehydrated Onion Powder – a convenient and flavor-packed solution for your agricultural needs! Sourced from top-quality onions, carefully dehydrated to retain maximum flavor and nutritional value, our onion powder is a versatile addition to your culinary arsenal.',
+        text:'A versatile seasoning essential for enhancing the flavors of soups, salads, pizzas, garlic bread, and sandwiches. Sourced from premium-quality onions and meticulously dehydrated, it adds depth and aroma to your culinary creations.',
         // text: "With the customer-focused approach and vast industrial experience, we are occupied in providing a wide assortment of Dehydrated White Onion Powder. We are manufacturers and suppliers of the finest quality of Dehydrated Onion Powder. Dehydrated Onion Powder is largely used in varied food preparations."
       },
       {
         title: "White Onion Powder",
         img: whiteOn,
-        text:'Introducing our premium White Onion Powder – a culinary essential and versatile gem for every farmers toolkit! Sourced from the finest white onions, carefully dehydrated to preserve flavor and nutritional goodness, our White Onion Powder is a game-changer for your agricultural endeavors.',
+        text:'Our White Onion powder is packed with antioxidants, it aids in cancer prevention, regulates blood sugar, supports digestion, boosts bone health, fights inflammation, bolsters immunity, enhances sleep, and nurtures a healthy heart.',
         // text: "Its characteristics strong flavour when raw makes it great addition on salad. Its sweet flavour when cooked is a key element for meats, poultry, seafood and all kind of side dishes."
       },
       {
         title: "Red Onion Powder",
         img: redOnion,
-        text:'Elevate your culinary creations with our Premium Red Onion Powder. Sourced from the finest red onions, this convenient and versatile powder adds rich flavor without the tears. Perfect for soups, stews, and marinades. Time-saving, long shelf life, and packed with health benefits. Farm-to-table transparency in every sprinkle. Upgrade your dishes with ease!',
+        text:'Elevate your culinary creations with our Red Onion Powder. Sourced from the finest red onions, this convenient and versatile powder adds rich flavor without the tears. Perfect for soups, stews, and marinades. Time-saving, long shelf life, and packed with health benefits. Farm-to-table transparency in every sprinkle. Upgrade your dishes with ease!',
         // ext: "Made from finest quality dehydrated Indian pink/red onions. The flavour is slightly stronger and sweeter than regular onion powder, and it is especially good to use in seasoning mixes, dry rubs and dressings.Elevate your culinary creations with our Premium Red Onion Powder. Sourced from the finest red onions, this convenient and versatile powder adds rich flavor without the tears. Perfect for soups, stews, and marinades. Time-saving, long shelf life, and packed with health benefits. Farm-to-table transparency in every sprinkle. Upgrade your dishes with ease!Elevate your culinary creations with our Premium Red Onion Powder. Sourced from the finest red onions, this convenient and versatile powder adds rich flavor without the tears. Perfect for soups, stews, and marinades. Time-saving, long shelf life, and packed with health benefits. Farm-to-table transparency in every sprinkle. Upgrade your dishes with ease!"
       }
   ];
